71498412083c1e847d4f6edc9ce472c1fd8a7f2a
cuentasena <josedavidlemusrestrepo5@gmail.com>
2025-09-18 19:04:14 -0500

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -1,11 +1,33 @@
 import java.util.Scanner;
-
 public class Matriz {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
-
+    public static void main(String[] xx) {
+        Scanner t = new Scanner(System.in);
+        System.out.print("Filas: ");
+        int n = t.nextInt();
+        System.out.print("Columnas: ");
+        int m = t.nextInt();
+        int[][] mat = new int[n][m];
+        System.out.println("Ingrese la matriz:");
+        int i = 0;
+        while (i < n) {
+            int j = 0;
+            while (j < m) {
+                mat[i][j] = t.nextInt();
+                j++;
+            }
+            i++;
+        }
+        System.out.println("Traspuesta:");
+        i = 0;
+        while (i < m) {
+            int j = 0;
+            while (j < n) {
+                System.out.print(mat[j][i] + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        t.close();
     }
-
 }
