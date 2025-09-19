c731b2803f6705bacc7aece49b910b04987a7988
cuentasena <josedavidlemusrestrepo5@gmail.com>
2025-09-18 19:05:42 -0500

    Update Conversorseg.java

--- Conversorseg.java
+++ Conversorseg.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
-
 public class Conversorseg {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Segundos: ");
+        int s = sc.nextInt();
+        int h = 0, m = 0;
+        h = s / 3600;
+        m = (s % 3600) / 60;
+        s = s % 60;
+        System.out.println(h + ":" + m + ":" + s);
         sc.close();
     }
 }
