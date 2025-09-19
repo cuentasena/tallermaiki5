a0f189da9aedeb4795ed72d883b8bad43f077a18
cuentasena <josedavidlemusrestrepo5@gmail.com>
2025-09-18 19:01:52 -0500

    Update Numeromagic.java

--- Numeromagic.java
+++ Numeromagic.java
@@ -1,10 +1,25 @@
-import java.util.Scanner;
+mport java.util.Scanner;
 
-public class Numeromagic {
+public class Main {
     public static void main(String[] args) {
+        // Numero magico: sumar dígitos hasta obtener 1
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Numero: ");
+        int n = sc.nextInt();
+        int orig = n;
+        while (n > 9) {
+            int suma = 0;
+            while (n > 0) {
+                suma = suma + (n % 10);
+                n = n / 10;
+            }
+            n = suma;
+        }
+        if (n == 1) {
+            System.out.println("Magico");
+        } else {
+            System.out.println("No magico");
+        }
         sc.close();
-
     }
 }
